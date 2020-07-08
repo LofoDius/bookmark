e6ce567f4ca8e763a23b01e6951a9588728d45fc
LofoD <evkaverin@gmail.com>
2020-07-08 13:11:40 +0300

    Complete card.xml

--- app/src/main/java/com/lofod/bookmark/MainActivity.java
+++ app/src/main/java/com/lofod/bookmark/MainActivity.java
@@ -3,13 +3,19 @@ package com.lofod.bookmark;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.MenuItemCompat;
 
-import android.app.SearchManager;
-import android.content.ComponentName;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.SearchView;
+import androidx.appcompat.widget.SearchView;
+
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
@@ -17,39 +23,40 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        invalidateOptionsMenu();
+        List<String> filters = Arrays.asList(getResources().getStringArray(R.array.filter_entries));
+        ArrayAdapter<String> filterAdapter = new ArrayAdapter<>(this, R.layout.fashionable_spinner_item, filters);
+        Spinner filterSpinner = findViewById(R.id.filterSpinner);
+        filterSpinner.setAdapter(filterAdapter);
+
+        List<String> sorts = Arrays.asList(getResources().getStringArray(R.array.sort_entries));
+        ArrayAdapter<String> sortAdapter = new ArrayAdapter<>(this, R.layout.fashionable_spinner_item, sorts);
+        Spinner sortSpinner = findViewById(R.id.sortSpinner);
+        sortSpinner.setAdapter(sortAdapter);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.main, menu);
-//
-//        MenuItem searchItem = menu.findItem(R.id.search_bar);
-//        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
-//        searchView.setOnQueryTextListener(this);
-//
-//        return true;
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main, menu);
-        SearchManager searchManager = (SearchManager) getSystemService(this.SEARCH_SERVICE);
-
-        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.search_bar));
-        //searchView.setSearchableInfo(searchManager.getSearchableInfo(new ComponentName(getApplicationContext(), SearchActivity.class)));
-        searchView.setMaxWidth(Integer.MAX_VALUE);
-        MenuItemCompat.expandActionView(menu.findItem(R.id.search_bar));
-        searchView.setIconifiedByDefault(true);
-        searchView.setIconified(false);
-        return false;
+        MenuItem searchItem = menu.findItem(R.id.search_bar);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setOnQueryTextListener(this);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onQueryTextSubmit(String query) {
-        return false;
+        Toast toast = Toast.makeText(this, "Working!", Toast.LENGTH_SHORT);
+        toast.show();
+        return true;
     }
 
     @Override
     public boolean onQueryTextChange(String newText) {
-        SearchView searchView = findViewById(R.id.search_bar);
         return true;
+    }
+
+    public void archiveClick(View view) {
+
     }
 }
